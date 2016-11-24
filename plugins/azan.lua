665929afee2f168cea056e266618aa198cc69498
Mc_Daived9 <mc.daived1996@gmail.com>
2016-11-24 16:35:54 +0300

    Update azan.lua

--- plugins/azan.lua
+++ plugins/azan.lua
@@ -1,6 +1,6 @@
 do
 local function run(msg, matches) 
-if matches[1] == 'kcikme' then 
+if matches[1] == 'kickme' then 
 local hash = 'kick:'..msg.to.id..':'..msg.from.id 
      redis:set(hash, "waite") 
       return 'عزيزي ('..msg.from.username..')\nهل انت متاكد من الخروج من المجموعة ارسل (نعم)✔️ لكي تخرج من المجموعة ان لم تكن متأكد من الخروج ارسل (لا) ✖️' 
@@ -11,7 +11,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
       if msg.text:match("^نعم$") and redis:get(hash) == "waite" then 
      redis:set(hash, "ok") 
    elseif msg.text:match("^لا$") and redis:get(hash) == "waite" then 
-   send_large_msg(get_receiver(msg), "تم الغاء الزالتك من المجموعة ") 
+   send_large_msg(get_receiver(msg), "تم الغاء الزالتك♻️ من المجموعة ") 
      redis:del(hash, true) 
 
       end 
@@ -20,7 +20,7 @@ local hash = 'kick:'..msg.to.id..':'..msg.from.id
     if redis:get(hash) then 
         if redis:get(hash) == "ok" then 
          channel_kick("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false) 
-         return 'لقد تم ازالتك من المجموعه('..msg.to.title..')⚠️' 
+         return 'لقد تم 📵ازالتك من المجموعه('..msg.to.title..')⚠️' 
         end 
       end 
     end 

9ed3f562c4f3daf304ebde66e2b82bccce63a389
Mc_Daived9 <mc.daived1996@gmail.com>
2016-11-25 15:56:02 +0300

    Update clean.lua

--- plugins/clean.lua
+++ plugins/clean.lua
@@ -1,31 +1,32 @@
-local function history(extra, suc, result)
-  for i=1, #result do
-    delete_msg(result[i].id, ok_cb, false)
-  end
-  if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" من الرسائل تم تنظيفها ', ok_cb, false)
-  else
-send_msg(extra.chatid, '♻️تم تنظيف الوصخ بنجاح, شنو هل وصخ ماكو بنكلادش هندي خليتهم يم الحاويه🚮☑️', ok_cb, false)
-end
-end
-local function run(msg, matches)
-  if matches[1] == 'clean' and is_sudo(msg) then
-    if msg.to.type == 'channel' then
-    if tonumber(matches[2]) > 100000 or tonumber(matches[2]) < 1 then
-        return "يمكنك وضع عدد 100000 رساله او اقل"
-      end
-      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
-    else
-      return ""
-    end
-  else
+local function history(extra, suc, result) 
+  for i=1, #result do 
+    delete_msg(result[i].id, ok_cb, false) 
+  end 
+  if tonumber(extra.con) == #result then 
+ send_msg(extra.chatid, '"'..#result..'" من الرسائل تم تنظيفها ', ok_cb, false)
+ else 
+ send_msg(extra.chatid, '♻️تم تنظيف الوصخ بنجاح, شنو هل وصخ ماكو بنكلادش هندي خليتهم يم الحاويه🚮☑️', ok_cb, false)
+  end 
+end 
+local function run(msg, matches) 
+  if matches[1] == 'clean' and is_owner(msg) then 
+    if msg.to.type == 'channel' then 
+      if tonumber(matches[2]) > 10000000000 or tonumber(matches[2]) < 1 then 
+        return "يمكنك تحديد عدد 10000000 رساله او اقل"
+      end 
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]}) 
+    else 
+      return "" 
+    end 
+  else 
     return "لا تلعب بكيفك️ روح اسبح♻️"
-  end
-end
+  end 
+end 
 
-return {
-    patterns = {
-        '^(clean) (%d*)$'
-    },
+return { 
+    patterns = { 
+        '^[Cc](lean) (%d*)$', 
+        '^[/!#](clean) (%d*)$' 
+    }, 
     run = run
-}
+} 
